b248020fd8c5b24ff54ae3a8b4392cba91a711aa
QuintinPK <qpknuf@gmail.com>
2025-04-02 00:15:57 -0400

    Update CheckIn.tsx
    AC2

--- src/pages/CheckIn.tsx
+++ src/pages/CheckIn.tsx
@@ -197,7 +197,7 @@ const CheckIn = () => {
             <div className="mt-6 bg-vacation-50 rounded-lg p-4">
               <h3 className="font-medium text-gray-800 mb-2">Important Notes</h3>
               <ul className="list-disc ml-5 space-y-1 text-gray-600">
-                <li>Mindful A/C Use:Please be respectful of neighbors - keep noise to a minimum between 11:00 PM and 7:00 AM.</li>
+                <li>Mindful A/C Use: Please be respectful of neighbors - keep noise to a minimum between 11:00 PM and 7:00 AM.</li>
                 <li>Please be respectful of neighbors - keep noise to a minimum between 11:00 PM and 7:00 AM.</li>
                 <li>Smoking is only allowed in outdoor areas, provided ashtrays are used. No open flames or candles indoors.</li>
                 <li>Pets are not permitted unless specifically arranged prior to booking.</li>
